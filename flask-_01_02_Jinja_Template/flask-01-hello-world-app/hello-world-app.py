798c530c712c0fa0c1ed7a6fe85107a2e7e6c9c8
yaman5891 <yaman5891@gmail.com>
2023-11-21 01:47:27 +0300

    second

---
+++ flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def head():
+    return "hello world"
+@app.route('/second')
+def second():
+    return 'this is second page'
+@app.route('/third')
+def third():
+    return 'this is third page'
+if __name__ == '__main__':
+    #app.run(debug=True, port=30000)
+    app.run(host= '0.0.0.0', port=8081)
